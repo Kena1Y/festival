c206b46d549db9ebb604364f6c41063e111c6c3f
Kena1Y <kenai.j.yard@gmail.com>
2025-06-17 13:24:10 +0200

    first commit

---
+++ include/connectdb.php
@@ -0,0 +1,8 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "module8.1_festivalapp";
+$pdo = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+
---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+
+<body>
+    test 11-06-2025 v2
+    <?php
+    include "./include/connectdb.php";
+
+    $stmt = $pdo->query("SELECT * FROM artists");
+    while ($row = $stmt->fetch()) {
+        echo $row['name'] . "<br />\n" . $row['description'];
+    }
+
+
+    ?>
+</body>
+
+</html>
